0f88ef33000b653f63e6e5641ec032eaf7bffc52
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2021-02-07 21:29:20 +0700

    Initialize project with IntellJ

---
+++ src/main/java/se2/hanu_hospital/HanuHospitalApplication.java
@@ -0,0 +1,13 @@
+package se2.hanu_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanuHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanuHospitalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/se2/hanu_hospital/ServletInitializer.java
@@ -0,0 +1,13 @@
+package se2.hanu_hospital;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HanuHospitalApplication.class);
+    }
+
+}
