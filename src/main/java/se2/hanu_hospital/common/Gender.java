0bc6c38186d527e0f0568ba3d5622f9514f973a2
Dao Anh Thanh <daoanhthanh.work@gmail.com>
2021-04-23 09:50:21 +0700

    restructured

--- src/main/java/se2/hanu_hospital/common/Gender.java
+++
@@ -1,7 +0,0 @@
-package se2.hanu_hospital.common;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
-}
--- src/main/java/se2/hanu_hospital/service/PatientRecordService.java
+++
@@ -1,4 +0,0 @@
-package se2.hanu_hospital.service;
-
-public class PatientRecordService {
-}
