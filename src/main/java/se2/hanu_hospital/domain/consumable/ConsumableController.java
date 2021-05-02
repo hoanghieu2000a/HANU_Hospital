d4dcf98fe3e43f934896e91d72f2d1f85e041641
Dao Anh Thanh <daoanhthanh.work@gmail.com>
2021-05-02 10:45:34 +0700

    modified:   src/main/java/se2/hanu_hospital/domain/consumable/ConsumableController.java 	new file:   src/main/java/se2/hanu_hospital/domain/facility/FacilityController.java 	new file:   src/main/java/se2/hanu_hospital/domain/facility/dto/CreateFacilityDTO.java 	new file:   src/main/java/se2/hanu_hospital/domain/facility/dto/UpdateFacilityDTO.java 	modified:   src/main/java/se2/hanu_hospital/domain/patient/PatientController.java

--- src/main/java/se2/hanu_hospital/domain/consumable/ConsumableController.java
+++ src/main/java/se2/hanu_hospital/domain/consumable/ConsumableController.java
@@ -1,5 +1,118 @@
 package se2.hanu_hospital.domain.consumable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import se2.hanu_hospital.domain.consumable.dto.CreateConsumableDTO;
+import se2.hanu_hospital.domain.consumable.dto.UpdateConsumableDTO;
+import se2.hanu_hospital.domain.consumable.entity.Consumable;
+
+@RestController
+@Tag(name = "Consumable Item Controller", description = "Consumable Item API")
 public class ConsumableController {
+    private final ConsumableService consumableService;
+
+    @Autowired
+    public ConsumableController(ConsumableService consumableService){
+        this.consumableService = consumableService;
+    }
+
+    @PostMapping("/add_item")
+    @Operation(summary = "Create a new consumable item")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Missing Request Parameter"),
+        @ApiResponse(responseCode = "422", description = "Input validation(s) failed"),
+        @ApiResponse(responseCode = "409", description = "Field value(s) already exists")
+    })
+    public Consumable create(@Valid @RequestBody CreateConsumableDTO createConsumableDTO){
+        return consumableService.create(createConsumableDTO);
+    }
+
+    @Operation(summary = "Find consumable item by keyword")
+    @GetMapping("/items")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Missing Request Parameter"),
+        @ApiResponse(responseCode = "422", description = "Input validation(s) failed"),
+        @ApiResponse(responseCode = "409", description = "Field value(s) already exists")
+    })
+    public ResponseEntity<List<Consumable>> findAll(@RequestParam(required = false) String keyword){
+        try {
+            List<Consumable> items = new ArrayList<Consumable>();
+      
+            if (keyword == null)
+              consumableService.findAll().forEach(items::add);
+            else
+              consumableService.findAllByName(keyword).forEach(items::add);
+      
+            if (items.isEmpty()) {
+              return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+      
+            return new ResponseEntity<>(items, HttpStatus.OK);
+          } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+          }
+    } 
+
+    @Operation(summary = "Find item by id")
+    @GetMapping("/item/{id}")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Missing Request Parameter"),
+        @ApiResponse(responseCode = "422", description = "Input validation(s) failed"),
+        @ApiResponse(responseCode = "409", description = "Field value(s) already exists")
+    })
+    public ResponseEntity<Consumable> findById(@PathVariable("id") Long id){
+        Consumable itemData = consumableService.getById(id);
+        if(itemData != null){
+            return new ResponseEntity<>(itemData, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @Operation(summary = "Update item")
+    @PutMapping("/item/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Missing Request Parameter"),
+            @ApiResponse(responseCode = "422", description = "Input validation(s) failed"),
+            @ApiResponse(responseCode = "409", description = "Field value(s) already exists")
+    })
+    public Consumable updateById(@PathVariable Long id, @Valid @RequestBody UpdateConsumableDTO updateConsumableDTO){
+        return consumableService.updateById(id, updateConsumableDTO);
+    }
+
+    @Operation(summary = "Delete a item by ID")
+    @DeleteMapping(value = "/item/{id}")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Missing Request Parameter"),
+        @ApiResponse(responseCode = "422", description = "Input validation(s) failed"),
+        @ApiResponse(responseCode = "409", description = "Field value(s) already exists")
+})
+    public void deleteByID(@PathVariable Long id) {
+        consumableService.deleteById(id);
+    }
+
+
+
+
+
     
+
 }
---
+++ src/main/java/se2/hanu_hospital/domain/facility/FacilityController.java
@@ -0,0 +1,5 @@
+package se2.hanu_hospital.domain.facility;
+
+public class FacilityController {
+    
+}
