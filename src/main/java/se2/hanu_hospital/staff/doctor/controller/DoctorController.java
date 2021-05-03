75c386c442452df9149e144c683534c219cb478e
Khanh Nguyen <coupdefart@gmail.com>
2021-05-03 22:21:21 +0700

    push nham branch nen push lai

---
+++ src/main/java/se2/hanu_hospital/staff/doctor/controller/DoctorController.java
@@ -0,0 +1,114 @@
+package se2.hanu_hospital.staff.doctor.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se2.hanu_hospital.Prescription.Prescription;
+import se2.hanu_hospital.staff.doctor.doctorMapper.DoctorDTO;
+import se2.hanu_hospital.staff.doctor.model.Doctor;
+import se2.hanu_hospital.staff.doctor.service.DoctorService;
+
+import java.io.IOException;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping(path = {"/api/doctor"})
+public class DoctorController {
+    private final DoctorService doctorService;
+
+    @Autowired
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
+    @GetMapping(path = "/getAll")
+    public ResponseEntity<?> getAll() {
+        try {
+            return new ResponseEntity<>(doctorService.getAllDoctors(), HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>("error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/get/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        try {
+            doctorService.getById(id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            doctorService.deleteById(id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/add")
+    public ResponseEntity<?> add(@RequestBody Doctor doctor) {
+        try {
+            doctorService.addDoctor(doctor);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/update/{id}")
+    public ResponseEntity<?> updateDoctorById(@PathVariable Long id, @RequestBody DoctorDTO doctorDTO) {
+        try {
+            doctorService.updateById(id, doctorDTO);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/prescribe")
+    public ResponseEntity<?> prescribe(@RequestBody Prescription prescription) {
+        try {
+            doctorService.prescribeMedicine(prescription);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/prescibe/update")
+    public ResponseEntity<?> updatePrescription(@RequestBody Prescription prescription) {
+        try {
+            doctorService.updatePrescription(prescription);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/prescibe/delete/{id}")
+    public ResponseEntity<?> deletePrescription(@PathVariable Long id) {
+        try {
+            doctorService.deletePrescription(id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/available")
+    public ResponseEntity<?> viewAvailableDoctor() {
+        try {
+            doctorService.getAvailableDoctors();
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ src/main/java/se2/hanu_hospital/staff/doctor/service/DoctorService.java
@@ -0,0 +1,76 @@
+package se2.hanu_hospital.staff.doctor.service;
+
+import org.springframework.stereotype.Service;
+import se2.hanu_hospital.Prescription.Prescription;
+import se2.hanu_hospital.Prescription.PrescriptionService;
+import se2.hanu_hospital.staff.doctor.doctorMapper.DoctorDTO;
+import se2.hanu_hospital.staff.doctor.doctorMapper.DoctorMapper;
+import se2.hanu_hospital.staff.doctor.model.Doctor;
+import se2.hanu_hospital.staff.doctor.repository.DoctorRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class DoctorService {
+    private final DoctorRepository doctorRepository;
+    private final PrescriptionService prescriptionService;
+    private DoctorMapper mapper;
+
+    public DoctorService(DoctorRepository doctorRepository, PrescriptionService prescriptionService) {
+        this.doctorRepository = doctorRepository;
+
+        this.prescriptionService = prescriptionService;
+    }
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.getAllDoctors();
+    }
+
+    public void addDoctor(Doctor doctor) {
+        doctorRepository.save(doctor);
+    }
+
+    public void updateById(Long id, DoctorDTO doctor) throws IOException {
+        if(!doctorRepository.existsById(id)){
+            throw new IllegalStateException("There is no doctor with that id!");
+        }
+
+        Doctor doctorInDB = (Doctor) doctorRepository.getDoctorById(id);
+        mapper.updateDoctorFromDto(doctor, doctorInDB);
+        doctorRepository.save(doctorInDB);
+    }
+
+    public void deleteById(Long id) {
+        doctorRepository.deleteById(id);
+    }
+
+    public Doctor getById(Long id) {
+        return (Doctor) doctorRepository.getDoctorById(id);
+    }
+
+    public void prescribeMedicine(Prescription prescription) throws Exception {
+        prescriptionService.add(prescription);
+    }
+
+    public void updatePrescription(Prescription prescription) throws IOException {
+        prescriptionService.update(prescription);
+    }
+
+    public void deletePrescription(Long id) throws IOException {
+        prescriptionService.delete(id);
+    }
+
+    public List<Doctor> getAvailableDoctors() {
+        return doctorRepository.getDoctorByAvailableIsTrue();
+    }
+
+    public void viewRecord() {
+
+    }
+
+    public void editRecord() {
+
+    }
+
+}
