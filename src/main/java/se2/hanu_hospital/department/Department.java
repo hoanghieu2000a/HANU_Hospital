e976a0e123b01df57c0d06fe935fd0ba6e177c0e
hakunhan <hakunhan@gmail.com>
2021-05-06 18:56:16 +0700

    add department

---
+++ src/main/java/se2/hanu_hospital/department/Department.java
@@ -0,0 +1,57 @@
+package se2.hanu_hospital.department;
+
+import se2.hanu_hospital.staff.Staff;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "INT(4) UNSIGNED ", precision = 4, updatable = false)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private Set<Staff> staff = new HashSet<>();
+
+    public Department(Long id, String name, Set<Staff> staff) {
+        this.id = id;
+        this.name = name;
+        this.staff = staff;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Staff> getStaff() {
+        return staff;
+    }
+
+    public void setStaff(Set<Staff> staff) {
+        this.staff = staff;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", staff=" + staff +
+                '}';
+    }
+}
